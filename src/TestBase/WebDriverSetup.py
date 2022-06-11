f601c6eb0e352db5c074e6490ad84bbbe24b64f6
ATLANTer <barstok05@bk.ru>
2022-06-11 13:36:26 +0300

    fix something bugs

--- src/TestBase/WebDriverSetup.py
+++ src/TestBase/WebDriverSetup.py
@@ -6,12 +6,11 @@ import urllib3
 class WebDriverSetup:
     def setup(self):
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-        self.driver = webdriver.Chrome("D:\chrmdriver\chromedriver.exe")
+        self.driver = webdriver.Chrome("../chromedriver.exe")
         self.driver.implicitly_wait(10)
         self.driver.maximize_window()
 
     def teardown(self):
         if self.driver is not None:
-            print("Cleanup of test environment")
             self.driver.close()
             self.driver.quit()
